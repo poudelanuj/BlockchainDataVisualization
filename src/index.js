22363356b93e7143b9b9d78f9cca6e7ac51cb47b
Anuj Poudel <poudel.01anuj@gmail.com>
2018-01-08 19:20:01 +0545

    Evening commit

--- src/index.js
+++ src/index.js
@@ -1,40 +1,52 @@
 import * as d3 from 'd3';
 
-var data = [4, 8, 15, 16, 23, 42];
+var data = [{
+    "sale": "202",
+    "year": "2000"
+}, {
+    "sale": "215",
+    "year": "2001"
+}, {
+    "sale": "179",
+    "year": "2002"
+}, {
+    "sale": "199",
+    "year": "2003"
+}, {
+    "sale": "134",
+    "year": "2003"
+}, {
+    "sale": "176",
+    "year": "2010"
+}];
 
-var width = 960,
-    height = 500;
 
-var y = d3.scaleLinear()
-    .range([height, 0]);
+var vis = d3.select("#visualisation"),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+    };
+var xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]);
 
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
+var yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]);
 
-d3.tsv("data.tsv", type, function(error, data) {
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+var xAxis =d3.axisBottom()
+    .scale(xScale);
 
-  var barWidth = width / data.length;
+var yAxis = d3.axisLeft()
+        .scale(yScale).orient("left");
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+vis.append("svg:g")
+  .call(xAxis);
 
-  bar.append("rect")
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", barWidth - 1);
+vis.append("svg:g")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+    .call(xAxis);
 
-  bar.append("text")
-      .attr("x", barWidth / 2)
-      .attr("y", function(d) { return y(d.value) + 3; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d.value; });
-});
-
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
-}
+vis.append("svg:g")
+    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+    .call(yAxis);
